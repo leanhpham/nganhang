b6a6cf137c38c88b07b7726860b69b8eb2597e1c
Hao <kehao12@gmail.com>
2018-11-11 11:26:28 +0700

    Update app/monhoc.php

---
+++ app/monhoc.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class monhoc extends Model
+{
+    protected $table="cauhoi";
+    protected $fillable=['id','tenMH','idNganh'];
+    public $timestamps=false;
+     public function chuong()
+     {
+     	return $this->hasMany('App\chuong','idMH','id');
+     }
+     public function nganh()
+     {
+     	return $this->belongsTo('App\nganh','idNganh','id');
+     }
+
+
+}
