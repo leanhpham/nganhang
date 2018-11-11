c563ab833cc11ab702129f33e4b25d1fdd0ecd84
Hao <kehao12@gmail.com>
2018-11-11 11:25:45 +0700

    Update app/cauhoi.php

---
+++ app/cauhoi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class cauhoi extends Model
+{
+    protected $table="cauhoi";
+    protected $fillable=['id','noiDungCH','ansA','ansB','ansC','ansD','dapAn','doKho','idChuong'];
+     public $timestamps=false;
+     public function chuong()
+     {
+     	return $this->belongsTo('App\chuong','idChuong','id');
+     }
+       public function chitietdethi()
+     {
+     	return $this->hasMany('App\chitietdethi','idCH','id');
+     }
+}
